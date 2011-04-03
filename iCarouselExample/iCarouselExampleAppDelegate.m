58e675d857538fcd711e25d3c7c5f7f4d9a7ba9f
demosthenese <demosthenese@5669d9cd-8514-4054-be0c-5edc5d6937f7>
2011-04-03 23:09:33 +0000

    Set correct company name in iCarousel project

--- iCarouselExample/iCarouselExampleAppDelegate.m
+++ iCarouselExample/iCarouselExampleAppDelegate.m
@@ -3,18 +3,15 @@
 //  iCarouselExample
 //
 //  Created by Nick Lockwood on 03/04/2011.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Charcoal Design. All rights reserved.
 //
 
 #import "iCarouselExampleAppDelegate.h"
-
 #import "iCarouselExampleViewController.h"
 
 @implementation iCarouselExampleAppDelegate
 
-
 @synthesize window=_window;
-
 @synthesize viewController=_viewController;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
--- iCarouselExample/iCarouselExampleViewController.h
+++ iCarouselExample/iCarouselExampleViewController.h
@@ -3,7 +3,7 @@
 //  iCarouselExample
 //
 //  Created by Nick Lockwood on 03/04/2011.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Charcoal Design. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
@@ -14,7 +14,6 @@
 
 @property (nonatomic, retain) IBOutlet iCarousel *carousel;
 @property (nonatomic, retain) IBOutlet UINavigationItem *navItem;
-
 
 - (IBAction)switchCarouselType;
 
