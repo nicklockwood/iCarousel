9c39425a911959bd692290a20561ff3052b42f26
Nick Lockwood <nick@charcoaldesign.co.uk>
2014-07-30 14:00:32 -0700

    Added Swift example

---
+++ Examples/Swift Example/SwiftExample/SwiftExample-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  SwiftExample-Bridging-Header.h
+//  SwiftExample
+//
+//  Created by Nick Lockwood on 30/07/2014.
+//  Copyright (c) 2014 Charcoal Design. All rights reserved.
+//
+
+#import "iCarousel.h"
---
+++ Examples/Swift Example/SwiftExample/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  SwiftExample
+//
+//  Created by Nick Lockwood on 30/07/2014.
+//  Copyright (c) 2014 Charcoal Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, iCarouselDataSource, iCarouselDelegate  {
+    
+    var items: [Int] = []
+    @IBOutlet var carousel : iCarousel!
+    
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        for i in 0...99
+        {
+            items.append(i)
+        }
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        carousel.type = .CoverFlow2
+    }
+    
+    func numberOfItemsInCarousel(carousel: iCarousel!) -> Int
+    {
+        return items.count
+    }
+    
+    func carousel(carousel: iCarousel!, viewForItemAtIndex index: Int, var reusingView view: UIView!) -> UIView!
+    {
+        var label: UILabel! = nil
+        
+        //create new view if no view is available for recycling
+        if (view == nil)
+        {
+            //don't do anything specific to the index within
+            //this `if (view == nil) {...}` statement because the view will be
+            //recycled and used with other index values later
+            view = UIImageView(frame:CGRectMake(0, 0, 200, 200))
+            (view as UIImageView!).image = UIImage(named: "page.png")
+            view.contentMode = .Center
+            
+            label = UILabel(frame:view.bounds)
+            label.backgroundColor = UIColor.clearColor()
+            label.textAlignment = .Center
+            label.font = label.font.fontWithSize(50)
+            label.tag = 1
+            view.addSubview(label)
+        }
+        else
+        {
+            //get a reference to the label in the recycled view
+            label = view.viewWithTag(1) as UILabel!
+        }
+        
+        //set item label
+        //remember to always set any properties of your carousel item
+        //views outside of the `if (view == nil) {...}` check otherwise
+        //you'll get weird issues with carousel item content appearing
+        //in the wrong place in the carousel
+        label.text = "\(items[index])"
+        
+        return view
+    }
+    
+    func carousel(carousel: iCarousel!, valueForOption option: iCarouselOption, withDefault value: CGFloat) -> CGFloat
+    {
+        if (option == .Spacing)
+        {
+            return value * 1.1
+        }
+        return value
+    }
+
+}
+
