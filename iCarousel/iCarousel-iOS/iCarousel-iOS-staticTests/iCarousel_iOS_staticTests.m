54c1b16afe501b55908f86d53aa3f0e8b7c14f3a
Aleksandr Dodatko <a.dodatko@healthjoy.com>
2015-06-17 17:31:22 +0300

    [carthage] Added a project with static and dynamic library targets

---
+++ iCarousel/iCarousel-iOS/iCarousel-iOS-staticTests/iCarousel_iOS_staticTests.m
@@ -0,0 +1,40 @@
+//
+//  iCarousel_iOS_staticTests.m
+//  iCarousel-iOS-staticTests
+//
+//  Created by Oleksandr Dodatko on 6/17/15.
+//  Copyright (c) 2015 healthjoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface iCarousel_iOS_staticTests : XCTestCase
+
+@end
+
+@implementation iCarousel_iOS_staticTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
