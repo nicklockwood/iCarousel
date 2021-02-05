0ec7525de66f70589d6680111042def2a766e1e2
Duy Tran <tphduy@gmail.com>
2021-02-05 20:30:30 +0700

    Prevent unnecessary directories to be included when opening swift package manifiest

---
+++ Tests/Events/Package.swift
@@ -0,0 +1,5 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+// This file prevents the actual package including this directory
+
+import PackageDescription
---
+++ Tests/iCarouselTests/iCarouselTests.swift
@@ -0,0 +1,4 @@
+import XCTest
+@testable import iCarousel
+
+final class iCarouselTests: XCTestCase {}
